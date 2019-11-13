e5be1caffac6d10ed1c4fef2604c46cc43674879
Kostas Koutoupis <kkoutoup@gmail.com>
2019-11-13 12:43:06 +0000

    Added gitignore and first test cases for Manager sub class

--- test_manager_sub_class.py
+++ test_manager_sub_class.py
@@ -6,78 +6,45 @@ John = Manager("John", "Smith", 50000, 3)
 Kristen = Manager("Kristen", "Stewart", 60000, 5, "Judy")
 
 # Test Manager subclass
-
-'''
-Create re-usable cases for tests that follow
-'''
-def test_manager_employees():
+def test_manager_employee_on_instance_creation():
   '''
   Test that employees are added upon instance creation
   '''
-  pass
-
-def test_manager_add_employee_that_already_exists():
-  '''
-  Should raise exception if employee has already been added
-  '''
-  pass
-
-def test_manager_add_employee_wrong_type():
-  '''
-  Raise exception if name of employee passed is empty string or wrong type
-  '''
-  pass
+  assert Kristen.employees == ["Judy"]
 
 def test_manager_add_employee():
   '''
   Testing method works
   '''
-  pass
+  Kristen.add_employee("Jason")
+  assert Kristen.employees == ["Judy", "Jason"]
+
+def test_manager_add_employee_that_already_exists():
+  '''
+  Raise exception if employee has already been added
+  '''
+  with pytest.raises(CompanyDatabaseError) as context:
+    Kristen.add_employee("Judy")
+  
+  # checking error message
+  assert str(context.value) == f"Employee 'Judy' already in the company's database"
+
+def test_manager_add_employee_wrong_type():
+  '''
+  Raise exception if name of employee passed is empty string or wrong type
+  '''
+  with pytest.raises(TypeError) as context:
+    Kristen.add_employee(2)
+
+  # checking error message
+  assert str(context.value) == "Please make sure employee name is a valid string"
 
 def test_manager_remove_employee_when_no_employees():
   '''
   Raise exception if employees list is empty
   '''
-  pass
-
-def test_manager_remove_employee_when_employee_not_there():
-  '''
-  Raise exception if employee hasn't been added
-  '''
-  pass
-
-def test_manager_remove_employee():
-  '''
-  Testing method works
-  '''
-  pass
-
-def test_manager_list_employees_when_list_is_empty():
-  '''
-  Raise exception if employees array is empty
-  '''
-  pass
-
-def test_manager_fullname():
-  '''
-  Testing inherited fullname method works
-  '''
-  pass
-
-def test_manager_email():
-  '''
-  Testing inherited email method works
-  '''
-  pass
-
-def test_manager_email_regex():
-  '''
-  Testing inherited method against regex
-  '''
-  pass
-
-def test_manager_apply_raise():
-  '''
-  Testing inherited apply_raise method works
-  '''
-  pass
+  with pytest.raises(CompanyDatabaseError) as context:
+    John.remove_employee("James")
+  
+  # checking error message
+  assert str(context.value) == "No employees have been added yet"
